b18e24489857ceab1f4b515cda63481b55b9ed8b
yeisonavilag <yeisonavilag@gmail.com>
2021-09-24 14:59:23 -0300

    Update01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './general/home/home.component';
+import { LoginComponent } from './general/login/login.component';
+import { RegistroComponent } from './general/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'ingreso', component: LoginComponent},
+  { path: 'registro', component: RegistroComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './compartida/header/header.component';
+import { AsideComponent } from './compartida/aside/aside.component';
+import { FooterComponent } from './compartida/footer/footer.component';
+import { HomeComponent } from './general/home/home.component';
+import { LoginComponent } from './general/login/login.component';
+import { RegistroComponent } from './general/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
