09d4430be80a1627c382ae60ae3743a36a59e853
yeisonavilag <yeisonavilag@gmail.com>
2021-09-27 17:09:38 -0300

    Update 3.5
    Agregada las validaciones del Login

--- src/app/general/login/login.component.ts
+++ src/app/general/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,65 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-  email: string| null=null;
-  password: string| null="";
+  email: string="";
+  password: string="";
+  //myForm: FormGroup;
+  //fb: FormBuilderBuilder;
 
+    
+/*
+  this.myForm = this.fb.group({
+    name: ['', [Validators.required]],
+    company: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    age: ['', [Validators.required]],
+    url: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+  */
 
   insertar():void{
+    
     console.log(" metodo insertar ...");
-    console.log("Email: "+this.email);
-    console.log("Password: "+this.password);
+    if( this.validarEmail() && this.validarPassword()){
+      console.log("Email: "+this.email);
+      console.log("Password: "+this.password);
+    }else {
+      console.log("No validado");
+    }
+    
   }
+
+  validarEmail(): boolean {
+    
+    if (this.email.trim().length > 0) {
+      console.log("El campo contiene un string válido que no son espacios");
+      if (this.email.indexOf('@', 0) == -1 || this.email.indexOf('.', 0) == -1) {
+        alert('El correo electrónico introducido no es correcto.');
+        console.log("Email: "+this.email);
+        return false;
+      } else
+
+        return true;
+    } 
+    else {
+      alert("El campo contiene espacios y está vacío");
+      return false;
+     }
+
+  }
+
+  validarPassword(): boolean{
+    if (this.password.trim().length==0) {
+      alert("Campo vacio");
+      return false;
+      } else if (this.password.length < 15) {
+      
+      console.log("Pass: "+this.password);
+        return true;
+      } else {
+        
+        return false;
+      }
+    }
 }
